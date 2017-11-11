6750c0cd8c7ecf7212ba20a97957ca793c79d5e4
mudox <mudoxisme@gmail.com>
2017-11-12 05:54:52 +0800

    Initial Commit

---
+++ LearnRY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnRY
+//
+//  Created by Mudox on 12/11/2017.
+//  Copyright © 2017 Mudox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
