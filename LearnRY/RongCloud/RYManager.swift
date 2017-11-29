7974efafa59cb751664a5ad02d97607b46f4862c
mudox <imudox@gmail.com>
2017-11-29 21:46:02 +0800

    Improve `RYManager`

--- LearnRY/RongCloud/RYManager.swift
+++ LearnRY/RongCloud/RYManager.swift
@@ -18,42 +18,47 @@ fileprivate let jack = Jack.with(levelOfThisFile: .debug)
 enum RYManagerError: Error {
   case connection(code: RCConnectErrorCode)
   case invalidToken
+  case currentUser(description: String)
 }
 
 class RYManager: NSObject {
 
+  // MARK: - Constants
   static let appKey = "82hegw5u8dz5x"
   static let appSecret = "2OWrRo0pGe5"
 
-  static var userToken: String? {
-    get {
-      let ud = UserDefaults.standard
-      return ud.string(forKey: "currentUserToken")
-    }
-    set {
-      let ud = UserDefaults.standard
-      ud.set(newValue, forKey: "currentUserToken")
-    }
-  }
-
+  // MARK: - Singleton
   static let shared = RYManager()
   private override init() { }
 
-  static func initSDK() {
-    let sdk = RCIM.shared()!
-    sdk.initWithAppKey(appKey)
-    sdk.userInfoDataSource = shared
-//    sdk.groupInfoDataSource = shared
-    sdk.enableTypingStatus = true
-//    sdk.enableMessageRecall = true
-//    sdk.enableMessageMentioned = true
-    sdk.enablePersistentUserInfoCache = true
+  // MARK: - User Token
+  static var cachedCurrentUserToken: String? {
+    get {
+      guard let dict = The.userDefaults.dictionary(forKey: "userTokens") else {
+        return nil
+      }
+
+      guard let id = FakeUser.current?.id else {
+        return nil
+      }
+
+      return dict[id] as? String
+    }
+  }
+
+  static func cacheToken(_ token: String, forUserName name: String) {
+    if var tokens = The.userDefaults.dictionary(forKey: The.UserDefaultsKey.cachedUserTokens) {
+      tokens[name] = token
+      The.userDefaults.set(tokens, forKey: The.UserDefaultsKey.cachedUserTokens)
+    } else {
+      The.userDefaults.set([name: token], forKey: The.UserDefaultsKey.cachedUserTokens)
+    }
   }
 
   var getCurrentUserToken: Single<String> {
     return Single<String>.create { single -> Disposable in
 
-      if let token = RYManager.userToken {
+      if let token = RYManager.cachedCurrentUserToken {
         single(.success(token))
         jack.debug("use cached token")
         return Disposables.create()
@@ -61,17 +66,17 @@ class RYManager: NSObject {
 
       jack.debug("request token from server")
 
-      let id =  FakeUsers.currentUser.userId!
-      let name =  FakeUsers.currentUser.name!
+      let id = FakeUser.current.id
+      let name = FakeUser.current.fullName
       let cancellable = MoyaProvider<RYTarget>().request(.getUserToken(id: id, name: name)) {
         result in
-        
+
         switch result {
         case .success(let response):
           do {
             let json = JSON(response.data)
             let token: String = try json.take(from: "token")
-            RYManager.userToken = token
+            RYManager.cacheToken(token, forUserName: name)
             single(.success(token))
           } catch {
             single(.error(error))
@@ -87,6 +92,19 @@ class RYManager: NSObject {
 
     } // Single.create
   }
+
+
+  static func initSDK() {
+    let sdk = RCIM.shared()!
+    sdk.initWithAppKey(appKey)
+    sdk.userInfoDataSource = shared
+//    sdk.groupInfoDataSource = shared
+    sdk.enableTypingStatus = true
+//    sdk.enableMessageRecall = true
+//    sdk.enableMessageMentioned = true
+    sdk.enablePersistentUserInfoCache = true
+  }
+
 
   func connectToServer(with token: String) -> Completable {
     return Completable.create { emit in
@@ -112,7 +130,7 @@ class RYManager: NSObject {
 extension RYManager: RCIMUserInfoDataSource {
 
   func getUserInfo(withUserId userId: String!, completion: ((RCUserInfo?) -> Void)!) {
-    completion( FakeUsers.user(withID: userId))
+    completion(FakeUser.all[userId]?.rcUserInfo)
   }
 
 }
