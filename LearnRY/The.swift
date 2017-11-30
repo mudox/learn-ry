bc3db432535a09505f4591d8f58312cddeed95c0
mudox <imudox@gmail.com>
2017-12-01 00:15:56 +0800

    Move common view controller the `The`

--- LearnRY/The.swift
+++ LearnRY/The.swift
@@ -11,16 +11,27 @@ import iOSKit
 extension The {
   
   static var appDelegate: AppDelegate { return The.app.delegate as! AppDelegate }
+  
+}
 
+extension The {
+  
+  static var chatNavigationController: UINavigationController {
+    return (The.mainWindow.rootViewController as! UITabBarController).viewControllers!.first as! UINavigationController
+  }
+  
+  static var rootTabBarController: UITabBarController {
+    return The.mainWindow.rootViewController as! UITabBarController
+  }
+  
 }
 
 extension The.UserDefaultsKey {
-
-
+  
   /// User name of current user
-  static let currentUserName = "currentUserName"
+  static let currentUserID = "currentUserName"
 
-  /// A `Dictionary` of `[userName: userToken]`
+  /// A `Dictionary` of `[userID: userToken]`
   static let cachedUserTokens = "cachedUserTokens"
 
 }
