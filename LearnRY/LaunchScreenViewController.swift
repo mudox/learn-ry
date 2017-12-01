df5d845f0bb925f4c46d3e8be050db4cb6f6735f
mudox <imudox@gmail.com>
2017-12-02 06:43:03 +0800

    Add `LaunchScreenViewController.swift`

---
+++ LearnRY/LaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenViewController.swift
+//  LearnRY
+//
+//  Created by Mudox on 01/12/2017.
+//  Copyright © 2017 Mudox. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+
+}
